8a7da47655e42171e8b9baaca21f7d1db2b1818b
cacxen <cacxxa@gmail.com>
2020-07-05 20:29:19 +0300

    add not found migration
    Недостающая миграция

---
+++ AuthenticationServer.Data/Migrations/20200705113912_AddSaltFieldInUserEntity.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace AuthenticationServer.Data.Migrations
+{
+    public partial class AddSaltFieldInUserEntity : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Salt",
+                table: "Users",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Salt",
+                table: "Users");
+        }
+    }
+}
